5037f05221d6a5ddd06ea488f46952629e0f864d
SMMainulHasan <mainulhasan7070@gmail.com>
2021-04-02 20:33:37 +0600

    home done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import ProductCard from '../ProductCard/ProductCard';
 import './Home.css';
 
 const Home = () => {
+    const [productCollection, setProductCollection] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5055/product')
+            .then(res => res.json())
+            .then(data => setProductCollection(data))
+    }, [])
     return (
         <>
             <Header />
             <div className="home">
-                <ProductCard />
+                {
+                    productCollection.map(pd => <ProductCard product={pd} key={pd._id} />)
+                }
             </div>
         </>
     );
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './ProductCard.css';
 import chips from '../../icons/image 37.png';
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+    const {imgUrl, productName, price} = props.product;
     return (
         <div className="product-cart">
-            <img src={chips} alt="" />
+            <img src={imgUrl} alt="" />
             <div className="product-detail">
-            <h4>This is the product name</h4>
+            <h4>{productName}</h4>
             <div className="price-container">
-                <h2>$299</h2>
+                <h2>${price}</h2>
                 <button className="buy-btn">Buy Now</button>
             </div>
             </div>
