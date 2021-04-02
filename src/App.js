24d3b370d5d011653a929dec10ce66acf3f46577
SMMainulHasan <mainulhasan7070@gmail.com>
2021-04-02 09:53:30 +0600

    context created

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { createContext, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,26 +9,33 @@ import Admin from "./components/Admin/Admin";
 import Home from "./components/Home/Home";
 import ManageProduct from "./components/ManageProduct/ManageProduct";
 
-
+const userContext = createContext();
 
 function App() {
+  const [user, setUser] = useState({
+    name:'',
+    email:'',
+    displayPic:''
+  });
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/admin">
-          <Admin />
-        </Route>
-        <Route path="/admin/manageProduct">
-          <ManageProduct />
-        </Route>
-        <Route path="/admin/addProduct">
-          <AddProduct />
-        </Route>
-      </Switch>
-    </Router>
+    <userContext.Provider value={[user, setUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/admin/manageProduct">
+            <ManageProduct />
+          </Route>
+          <Route path="/admin/addProduct">
+            <AddProduct />
+          </Route>
+        </Switch>
+      </Router>
+    </userContext.Provider>
   );
 }
 
